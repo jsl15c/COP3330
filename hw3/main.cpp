076a9a6ad725b688ffefdd632ab636c2a3cf4124
Jarrod Luca <jarrodluca@gmail.com>
2017-10-06 13:57:10 -0400

    add gitignore

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -12,23 +12,23 @@ int main()
    cout << "d4 = " << d4 << '\n';
    cout << "d5 = " << d5 << '\n';
 
-   cout << "Enter first Distance object (MILES,YDS,FT,IN):  ";
-   cin >> d1;
-
-   cout << "Enter second Distance object (MILES,YDS,FT,IN):  ";
-   cin >> d2;
-
-   cout << "\n\n";
-   cout << d1 << " + " << d2 << " = " << d1 + d2 << '\n';
-   cout << d1 << " - " << d2 << " = " << d1 - d2 << "\n\n";
-
-   if (d1 < d2) 	cout << d1 << " <  " << d2 << " is TRUE\n";
-   if (d1 > d2) 	cout << d1 << " >  " << d2 << " is TRUE\n";
-   if (d1 <= d2) 	cout << d1 << " <= " << d2 << " is TRUE\n";
-   if (d1 >= d2) 	cout << d1 << " >= " << d2 << " is TRUE\n";
-   if (d1 == d2) 	cout << d1 << " == " << d2 << " is TRUE\n";
-   if (d1 != d2) 	cout << d1 << " != " << d2 << " is TRUE\n\n";
-
-   cout << d1 << " + 654321 = " << d1 + 654321 << '\n';
-   cout << d2 << " + 15263748 = " << d2 + 15263748 << '\n';
+  //  cout << "Enter first Distance object (MILES,YDS,FT,IN):  ";
+  //  cin >> d1;
+   //
+  //  cout << "Enter second Distance object (MILES,YDS,FT,IN):  ";
+  //  cin >> d2;
+   //
+  //  cout << "\n\n";
+  //  cout << d1 << " + " << d2 << " = " << d1 + d2 << '\n';
+  //  cout << d1 << " - " << d2 << " = " << d1 - d2 << "\n\n";
+   //
+  //  if (d1 < d2) 	cout << d1 << " <  " << d2 << " is TRUE\n";
+  //  if (d1 > d2) 	cout << d1 << " >  " << d2 << " is TRUE\n";
+  //  if (d1 <= d2) 	cout << d1 << " <= " << d2 << " is TRUE\n";
+  //  if (d1 >= d2) 	cout << d1 << " >= " << d2 << " is TRUE\n";
+  //  if (d1 == d2) 	cout << d1 << " == " << d2 << " is TRUE\n";
+  //  if (d1 != d2) 	cout << d1 << " != " << d2 << " is TRUE\n\n";
+   //
+  //  cout << d1 << " + 654321 = " << d1 + 654321 << '\n';
+  //  cout << d2 << " + 15263748 = " << d2 + 15263748 << '\n';
 }
